783ae052641f10e81c428d4abcd9a323150ea662
sklab-dev1 <bidisha.pal.uk@gmail.com>
2022-07-27 18:31:21 +0530

    deleted old firebase code from comment, and added some comments to understand the code

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,23 +3,24 @@ import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import { deleteDoc, doc, db } from '../firebase';
 import React from 'react';
 import './Todo.css'
-//const Todo = ({ todo }) => {
+// arr is a prop that represents "it", that is each todo from the "todos" arrey
+// we will do whatever we need to do with each todo (here it is arr, and use the component at App.js)
   const Todo = ({ arr }) =>{
     return (
     <List className="todo__list">
       <ListItem>
         <ListItemAvatar />
         <ListItemText 
+        // inside the todos arrey, each todo is referred as arr here
+        // at todos arrey, we set doc.data from firestore at "item" feild / variable.
+        //inside the item (firebase doc.data), there was 2 feilds, "todo" and "createdAt"
+        //we are collecting the "todo" field values here
         primary={arr.item.todo}
         secondary={arr.item.todo} 
         />
       </ListItem>
 
-     {/* firebase old code
-     <DeleteForeverIcon
-     onClick={() => {db.collection('todos').doc(arr.id).delete()}}
-     />
-     */}
+     {/* Delete button goes here */}
 
       <DeleteForeverIcon fontSize='large'
        onClick={() => {
